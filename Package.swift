9da3cc82acb0c2722805a393237826e0d275336a
泽i <zhangzh@eagersoft.cn>
2021-11-16 18:06:12 +0800

    分享

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Share",
+    platforms: [.iOS(.v10)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -13,16 +14,17 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+         .package(url: "https://github.com/ze230123/QQApi.git", branch: "main"),
+         .package(url: "https://github.com/ze230123/WXApi.git", branch: "main"),
+//         .package(name: "WXApi", path: "/Users/youzy01/Github/WXApi"),
+//         .package(name: "QQApi", path: "/Users/youzy01/Github/QQApi")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Share",
-            dependencies: []),
-        .testTarget(
-            name: "ShareTests",
-            dependencies: ["Share"]),
+            dependencies: ["QQApi", "WXApi"]),
     ]
 )
+
--- Sources/Share/Share.swift
+++ Sources/Share/Share.swift
@@ -1,6 +1,151 @@
-public struct Share {
-    public private(set) var text = "Hello, World!"
 
-    public init() {
+import UIKit
+import WXApi
+import QQApi
+
+public enum ShareResult {
+    case success
+    case failure
+}
+
+enum ShareError: Error {
+    case cancel
+    case failure
+    case imageNil
+}
+
+public class Share {
+    static func registerApp(wxCongigure: WXConfiguration, qqAppId: String) {
+        WXApiManager.register(for: wxCongigure)
+        QQApi.register(for: qqAppId)
+    }
+
+    public static func send(_ request: Share.Request, complation: ((ShareResult) -> Void)?) {
+        do {
+            switch request.platform {
+            case .wxFriend:
+                let image = try request.image.asImage()
+                let request = WXApi.friendRequest(url: request.url, title: request.title, description: request.description, image: image)
+                WXApiManager.share(request) { result in
+                    switch result {
+                    case .success:
+                        complation?(.success)
+                    case .failure:
+                        complation?(.failure)
+                    }
+                }
+            case .wxTimeline:
+                    let image = try request.image.asImage()
+                    let request = WXApi.friendRequest(url: request.url, title: request.title, description: request.description, image: image)
+                    WXApiManager.share(request) { result in
+                        switch result {
+                        case .success:
+                            complation?(.success)
+                        case .failure:
+                            complation?(.failure)
+                        }
+                    }
+            case .qqFriend:
+                let image = try request.image.asImage()
+                let request = QQApi.shareRequest(url: request.url, title: request.title, description: request.description, imageData: image.pngData())
+                QQApi.shareQQ(req: request) { result in
+                    switch result {
+                    case .success:
+                        complation?(.success)
+                    case .failure:
+                        complation?(.failure)
+                    }
+                }
+            case .qZone:
+                let image = try request.image.asImage()
+                let request = QQApi.shareRequest(url: request.url, title: request.title, description: request.description, imageData: image.pngData())
+                QQApi.shareQZone(req: request) { result in
+                    switch result {
+                    case .success:
+                        complation?(.success)
+                    case .failure:
+                        complation?(.failure)
+                    }
+                }
+            }
+        } catch {
+            debugPrint("分享 error", error.localizedDescription)
+            complation?(.failure)
+        }
+    }
+
+    public static func send(_ request: Share.MiniProgramRequest, complation: ((ShareResult) -> Void)?) {
+        do {
+            let image = try request.image?.asImage()
+            let request = WXApi.miniRequest(path: request.path, userName: request.username, title: request.title, description: request.description, image: image)
+            WXApiManager.share(request) { reuslt in
+                switch reuslt {
+                case .success:
+                    complation?(.success)
+                case .failure:
+                    complation?(.failure)
+                }
+            }
+        } catch {
+            debugPrint("分享小程序 error", error.localizedDescription)
+            complation?(.failure)
+        }
+    }
+}
+
+public extension Share {
+    enum Platform {
+        case wxFriend
+        case wxTimeline
+        case qqFriend
+        case qZone
+    }
+
+    struct Request {
+        var platform: Share.Platform
+        /// 消息标题
+        var title: String
+        /// 描述内容
+        var description: String
+        /// 分享的链接
+        var url: String
+        /// 缩略图
+        var image: ShareImage
+    }
+
+    struct MiniProgramRequest {
+        /// 小程序的页面路径
+        var path: String
+        /// 小程序的userName
+        var username: String
+        /// 小程序新版本的预览图
+        var image: ShareImage?
+        /// 消息标题
+        var title: String
+        /// 描述内容
+        var description: String
+    }
+}
+
+enum ShareImage {
+    case image(UIImage?)
+    case url(String)
+}
+
+extension ShareImage {
+    func asImage() throws -> UIImage {
+        switch self {
+        case .image(let optional):
+            guard let image = optional else {
+                throw ShareError.imageNil
+            }
+            return image
+        case .url(let string):
+            let data = try Data(contentsOf: URL(string: string)!)
+            guard let image = UIImage(data: data) else {
+                throw ShareError.imageNil
+            }
+            return image
+        }
     }
 }
